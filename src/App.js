5d4cf6958d04e9fabc548efe00df023f5947ac17
Patric Khiev <pat@Patrics-MBP.attlocal.net>
2018-10-23 00:25:45 -0500

    missing sticky navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="landing_page">
+          <div className="App_header">
+            <div className="header_container_left">
+              <p>Start Bootstrap</p>
+            </div>
+            <div className="header_container_right">
+              <p>SERVICES</p>
+              <p>PORTFOLIO</p>
+              <p>ABOUT</p>
+              <p>TEAM</p>
+              <p>CONTACT</p>
+            </div>
+          </div>
+          <div className="body">
+            <div className="inner_body">
+              <h2>Welcome To Our Studio!</h2>
+              <h1>IT'S NICE TO MEET YOU</h1>
+              <button className="tell_button">TELL ME MORE</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
